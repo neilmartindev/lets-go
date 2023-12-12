7dc7404ce73f6299ebbe97fb804836874b1c3ac0
Neil Martin <neilmartindotdev@protonmail.com>
2023-12-12 22:58:17 +0000

    page 70 - custom structured logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define what port the web server will run on
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Parse the command-line flag
+	flag.Parse()
+
 	mux := http.NewServeMux()
+
+	// Create a file server which servers files out of the /static/ directory
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() to register the file server as a handler for all URL paths within "/static/"
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
+	log.Print("Starting server on", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	// Pass the addr pointer to the http.ListenAndServe()
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
